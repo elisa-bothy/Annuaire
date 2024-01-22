de40669636b0fc7d5ac5a107b36ac70d57fd54d4
Elisa Bothy <elisa.bothy@gmail.com>
2024-01-22 10:45:08 +0100

    Version initiale

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,24 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLink } from '@angular/router';
+import { UpperCasePipe, DatePipe} from '@angular/common';
+import { AproposComponent } from "./apropos/apropos.component";
+import { WelcomeComponent } from "./welcome/welcome.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [RouterOutlet, RouterLink, UpperCasePipe, DatePipe, AproposComponent, WelcomeComponent]
 })
 export class AppComponent {
   title = 'Annuaire';
+  oeuvre = {
+    titre : 'Star Wars',
+    duree : 240,
+    dateSortie : new Date(),
+
+  }
 }
+
+
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { AproposComponent } from './apropos/apropos.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '',component: WelcomeComponent},
+    {path: 'apropos',component: AproposComponent}
+];
